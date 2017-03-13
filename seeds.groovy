9d88181e6a424a3cd2d1c719a235171ee05eb217
Kovalyoff <vitaliy.kovalyoff@gmail.com>
2017-03-13 18:46:03 +0200

    Create seeds.groovy

---
+++ seeds.groovy
@@ -0,0 +1,3 @@
+pipelineJob('example') {  scm {    git ('https://github.com/pingwin4ik/dsl.git')  }    triggers {     scm('H/2 * * * *')  }     definition {        cps {            script(readFileFromWorkspace('jenkinsfile'))            sandbox()        }    }
+
+}
