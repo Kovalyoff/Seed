dea58640dd28c070f597ab3ae0a342a5520bc572
Kovalyoff <vitaliy.kovalyoff@gmail.com>
2017-03-13 18:47:50 +0200

    Update seeds.groovy

--- seeds.groovy
+++ seeds.groovy
@@ -1,3 +1,11 @@
-pipelineJob('example') {  scm {    git ('https://github.com/pingwin4ik/dsl.git')  }    triggers {     scm('H/2 * * * *')  }     definition {        cps {            script(readFileFromWorkspace('jenkinsfile'))            sandbox()        }    }
+pipelineJob('example') {  
+  scm {    git ('https://github.com/pingwin4ik/dsl.git')  }   
+  triggers {     scm('H/2 * * * *')  }    
+  definition {       
+    cps {            script(readFileFromWorkspace('jenkinsfile')
+    )            
+         sandbox()        
+        }    
+  }
 
 }
